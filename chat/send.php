a433fd10805e66a3f8deb250f3314dddc7e19ef3
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 05:47:36 +0100

    Update send.php

--- chat/send.php
+++ chat/send.php
@@ -3,12 +3,15 @@ require 'vendor/autoload.php';
 
 $pusher = new Pusher\Pusher(
     'c922bfca140061b3ea91',
-    '4f39fcf9d33d7dcb174a',        // ← garde le secret privé
+    '4f39fcf9d33d7dcb174a',
     '2130604',
     ['cluster' => 'eu']
 );
 
 $data = json_decode(file_get_contents('php://input'), true);
-$pusher->trigger('chat', 'message', ['text' => $data['text']]);
+$pusher->trigger('chat', 'message', [
+    'text' => $data['text'],
+    'userId' => $data['userId']
+]);
 
 echo json_encode(['status' => 'ok']);
