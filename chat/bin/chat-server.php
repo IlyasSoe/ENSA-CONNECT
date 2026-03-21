2558a822799257d12b7e1d392e53aa255d840887
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 04:49:25 +0100

    Update chat-server.php

--- chat/bin/chat-server.php
+++ chat/bin/chat-server.php
@@ -1,13 +1,15 @@
 <?php
-
-
     use Ratchet\Server\IoServer;
     use Ratchet\Http\HttpServer;
     use Ratchet\WebSocket\WsServer;
     use MyApp\Chat;
-
+    
     require dirname(__DIR__) . "/vendor/autoload.php";
-
+    
+    // Désactiver la détection automatique du PORT par ReactPHP
+    putenv('PORT=9090');
+    $_ENV['PORT'] = '9090';
+    
     $server = IoServer::factory(
         new HttpServer (
             new WsServer (
@@ -16,7 +18,6 @@
         ),
         9090
     );
-
-
+    
     $server->run();
 ?>
