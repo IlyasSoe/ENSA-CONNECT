304a8c12b0ee640bbedaef9992628ba0d093e1bd
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 04:13:43 +0100

    Update start.php

--- start.php
+++ start.php
@@ -1,7 +1,5 @@
 <?php
-$port = getenv('PORT') ?: 8080;
+$port = getenv('PORT') ?: '8080';
 echo "Starting on port: $port\n";
 shell_exec('php chat/bin/server.php > /dev/null 2>&1 &');
-$server = new \React\Http\HttpServer(function() {});
-// lancer le serveur built-in
 passthru("php -S 0.0.0.0:$port -t chat/");
