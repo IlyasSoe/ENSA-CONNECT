34da936f9931bf96b2d54a3a7809163d29ee128b
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 16:29:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 cd C:\xampp\htdocs\me\myApp\ENSA-CONNECT
 ls
 git status
-```
 
-Est-ce que tu vois `start.sh` dans la liste ? Si non, recrée-le manuellement avec **Notepad** :
-
-1. Ouvre Notepad
-2. Colle exactement :
-```
 #!/bin/bash
 php chat/bin/server.php &
 exec php -S 0.0.0.0:$PORT -t chat/
