b159567326124ab4ebcafbaa5100001158450926
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 04:09:15 +0100

    Update and rename start.sh to start.php

---
+++ start.php
@@ -0,0 +1,7 @@
+<?php
+$port = getenv('PORT') ?: 8080;
+echo "Starting on port: $port\n";
+shell_exec('php chat/bin/server.php > /dev/null 2>&1 &');
+$server = new \React\Http\HttpServer(function() {});
+// lancer le serveur built-in
+passthru("php -S 0.0.0.0:$port -t chat/");
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "PORT is: $PORT"
-php chat/bin/server.php &
-exec php -S 0.0.0.0:$PORT -t chat/
