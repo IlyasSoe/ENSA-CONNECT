50b9627d54c381dd3f82ce9fb9b7355999746703
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 04:22:08 +0100

    Update start.php

--- start.php
+++ start.php
@@ -2,4 +2,4 @@
 $port = getenv('PORT') ?: '8080';
 echo "Starting on port: $port\n";
 shell_exec('php chat/bin/server.php > /dev/null 2>&1 &');
-passthru("php -S 0.0.0.0:$port -t chat/");
+passthru("php -S 0.0.0.0:" . $port . " -t chat/");
