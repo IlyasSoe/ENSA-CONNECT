800f87ceedbe6988d00bc3050c4e2bbf891aff7b
Ilyas Oufaraji <102984136+IlyasSoe@users.noreply.github.com>
2026-03-21 04:47:13 +0100

    Update start.php

--- start.php
+++ start.php
@@ -1,14 +1,6 @@
 <?php
-$port = getenv('PORT') ?: '8080';
+$port = getenv('WEB_PORT') ?: '8080';
 echo "PORT: $port\n";
-
-// Lancer Ratchet en arrière-plan sur port fixe
 shell_exec('php chat/bin/chat-server.php > /dev/null 2>&1 &');
-
-// Attendre que Ratchet démarre
 sleep(1);
-
-// Lancer serveur web
-$cmd = "php -S 0.0.0.0:" . intval($port) . " -t chat/";
-echo "Running: $cmd\n";
-passthru($cmd);
+passthru("php -S 0.0.0.0:" . intval($port) . " -t chat/");
